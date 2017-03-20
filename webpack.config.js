9fc575979f5628b2a59ab692b410471136452105
swapnilrajepatil <swapnilrajepatil@users.noreply.github.com>
2017-03-20 15:49:54 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+var config = {
+   entry: './main.js',
+	
+   output: {
+      path: __dirname + "/dist/js", // or path: path.join(__dirname, "dist/js"),
+      filename: 'index.js',
+   },
+	
+   devServer: {
+      inline: true,
+      port: 7777
+   },
+	
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+				
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+
+module.exports = config;
+
+/*
+var path = require('/dist/js');
+var webpack = require('webpack');
+ 
+module.exports = {
+  entry: './main.js',
+  output: { path: __dirname, filename: 'index.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      { 
+        test: /\.css$/, 
+        loader: "style-loader!css-loader" 
+      },
+      { 
+        test: /\.png$/, 
+        loader: "url-loader?limit=100000" S
+      },
+      { 
+        test: /\.jpg$/, 
+        loader: "file-loader" 
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'file'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
+      }
+    ]
+  },
+};*/
